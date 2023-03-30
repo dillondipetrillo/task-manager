d4a0afb58ae0261bced07a2898a9e8e44ed3016c
dillondipetrillo <dillonrdipetrillo@gmail.com>
2023-03-30 14:02:31 -0400

    added more sidebar styles and some temp js to see click events

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 import "./styles/styles.scss";
+
+const addProjectIcon = document.querySelector(".add-project-icon");
+const addProjectForm = document.querySelector(".add-project-form");
+const addProjectCancelBtn = document.querySelector("button.cancel");
+
+addProjectIcon.addEventListener("click", function () {
+  if (addProjectIcon.classList.contains("open")) {
+    addProjectIcon.classList.remove("open");
+    addProjectForm.classList.remove("open");
+  } else {
+    addProjectIcon.classList.add("open");
+    addProjectForm.classList.add("open");
+  }
+});
+
+addProjectCancelBtn.addEventListener("click", function () {
+  addProjectIcon.classList.remove("open");
+  addProjectForm.classList.remove("open");
+});
