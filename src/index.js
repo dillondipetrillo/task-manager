d018f6560fa2f4df3d27782dd5368256b6891a66
dillondipetrillo <dillonrdipetrillo@gmail.com>
2023-03-31 01:24:08 -0400

    add more style and functionality to sidebar

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import "./styles/styles.scss";
 const addProjectIcon = document.querySelector(".add-project-icon");
 const addProjectForm = document.querySelector(".add-project-form");
 const addProjectCancelBtn = document.querySelector("button.cancel");
+const projectEditIcon = document.querySelector(".project-edit-icon");
+const projectEditDelete = document.querySelector(".project-edit-delete");
+const spans = document.querySelectorAll(".project-edit-delete span");
+
+const [span1, span2] = spans;
 
 addProjectIcon.addEventListener("click", function () {
   if (addProjectIcon.classList.contains("open")) {
@@ -17,4 +22,12 @@ addProjectIcon.addEventListener("click", function () {
 addProjectCancelBtn.addEventListener("click", function () {
   addProjectIcon.classList.remove("open");
   addProjectForm.classList.remove("open");
+});
+
+projectEditIcon.addEventListener("click", function () {
+  if (projectEditDelete.classList.contains("open")) {
+    projectEditDelete.classList.remove("open");
+  } else {
+    projectEditDelete.classList.add("open");
+  }
 });
